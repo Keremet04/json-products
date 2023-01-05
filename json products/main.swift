3bf736a1f64b5e91be049436cfb6e538dc2af174
Keremet Dogdurbaeva <dogdurbaeva04@bk.ru>
2023-01-05 16:59:59 +0600

    home

--- json products/main.swift
+++ json products/main.swift
@@ -5,7 +5,52 @@
 //  Created by Керемет  on 5/1/23.
 //
 
+
 import Foundation
 
-print("Hello, World!")
+let products = """
 
+[
+{
+    "name": "chicken",
+    "price": 700,
+    "weight": "1 kg"
+},
+
+{
+    "name": "tomatos",
+    "price": 150,
+    "weight": "1 kg"
+}
+]
+"""
+
+struct Products: Codable {
+    
+    var name: String
+    var price: Int?
+    var weight: String
+}
+
+class Shop: Decodable {
+    private var prod: [Products] = [] {
+        didSet {
+            print(prod)
+        }
+    }
+    
+    func getProducts() {
+        let productsData = Data(products.utf8)
+        let productsDecoder = JSONDecoder()
+        
+        do {
+            let decodedProducts = try productsDecoder.decode([Products].self, from: productsData)
+           prod = decodedProducts
+        }catch{
+            print("Error: \(error.localizedDescription)")
+        }
+    }
+}
+
+let controller = Shop()
+controller.getProducts()
